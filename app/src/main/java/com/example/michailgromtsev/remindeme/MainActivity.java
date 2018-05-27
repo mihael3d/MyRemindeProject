46b07ed79431beced91eb45a71b2651bbe6768f9
Gromtsev Mikhail <m.gromtsev@gmail.com>
2018-05-27 15:53:18 +0600

    [#task]Added Toolbar and search button

--- app/src/main/java/com/example/michailgromtsev/remindeme/MainActivity.java
+++ app/src/main/java/com/example/michailgromtsev/remindeme/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.michailgromtsev.remindeme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intitTolbar();
+    }
+
+    private void intitTolbar() {
+        mToolbar = findViewById(R.id.toolbar);
+        mToolbar.setTitle(R.string.app_name);
+
+        mToolbar.inflateMenu(R.menu.menu);
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return false;
+            }
+        });
     }
 }
